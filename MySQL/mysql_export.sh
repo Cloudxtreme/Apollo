aa5703f90a83463c942d93a9197ab00d7e387c2e
Sergiu Tot <sergiu.it@gmail.com>
2015-11-18 16:57:56 +0200

    Create mysql_export.sh

---
+++ MySQL/mysql_export.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DBS=0 # Total databasses
+TBS=0 # Total tables
+
+# Current date, for backup folder
+CDATE=`date +%Y-%m-%d`
+WKDIR="/backup/${CDATE}"
+
+mkdir -p $WKDIR
+
+for DB in $(mysql -sNe "SHOW DATABASES;")
+do
+    # Exclude metadata
+    if [[ ${DB} != 'information_schema' ]]
+    then
+        # Create a folder for each database
+        echo -e "Database: ${DB}..."
+        DBDIR="${WKDIR}/${DB}"
+        mkdir -p ${DBDIR}
+        DBS=$((${DBS}+1))
+        # Loop through the tables
+        for TBL in `mysql ${DB} -sNe "SHOW TABLES;"`
+        do
+            # Export the tables and compress the result
+            echo -n "  Exporting table ${DB}.${TBL}..."
+            mysqldump --opt ${DB} ${TBL} | gzip -9 > "${DBDIR}/${TBL}.sql.gz"
+            if [ $? -eq 0 ]; then echo "Done."; TBS=$((${TBS}+1)); else echo "Failed."; fi;
+        done
+    fi
+done
+
+# Print the results
+echo
+printf "********************************************\n"
+printf "**** %-34s ****\n"                              "Exported databases: ${DBS}"
+printf "**** %-34s ****\n"                              "Exported tables: ${TBS}"
+printf "********************************************\n"
